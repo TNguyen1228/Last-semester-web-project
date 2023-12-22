c0462ee6543971f9eb2316c0512a17a19043e649
TNguyen1228 <tuan1056@gmail.com>
2023-12-23 04:10:40 +0700

    update api endpoint
    Add api endpoint
    add login form, login endpoint

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
-from fastapi import FastAPI, Form, HTTPException, Request, Response
+from fastapi import Depends, FastAPI, Form, HTTPException, Request, Response
 from fastapi.staticfiles import StaticFiles
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, RedirectResponse
 import mysql.connector
 from fastapi.templating import Jinja2Templates
+from starlette.middleware.sessions import SessionMiddleware
+import secrets
+import string
+
+def generate_secret_key(length=20):
+    alphabet = string.ascii_letters + string.digits + string.punctuation
+    secret_key = ''.join(secrets.choice(alphabet) for _ in range(length))
+    return secret_key
+# Secret key for session encryption
+SECRET_KEY = generate_secret_key()
 
 app=FastAPI()
 
 templates=Jinja2Templates(directory='./')
+
 app.mount("/assets", StaticFiles(directory="assets"))
 
+# Configure session middleware
+app.add_middleware(SessionMiddleware, secret_key=SECRET_KEY, session_cookie="session-id", max_age=1800)
+
+#Configure connect 
 customer = mysql.connector.connect(
     host="",
     user="custom",
@@ -69,24 +84,45 @@ async def submit_form(name: str = Form(),
     customer.commit()
     return Response(status_code=200)
 
-
-users_db = {
-    "admin": {"username": "admin", "password": "stupidThingbaby"},
-    "nhanvien": {"username": "nhanvien", "password": "banlanhat"},
+users = {
+    "admin": {"username": "admin", "password": "admin"}
     # Add more users as needed
 }
 
-@app.post('/login-check')
-async def login(username: str=Form(), password: str=Form()):
+# @app.post('/login-check')
+# async def login(username: str=Form(), password: str=Form()):
 
-    # Check if the user exists in the database
-    if username in users_db and users_db[username]["password"] == password:
+#     # Check if the user exists in the database
+#     if username in users_db and users_db[username]["password"] == password:
+#         return Response(status_code=200)
+#     else:
+#         raise HTTPException(status_code=401)
+    
+# Dependency to access the session
+def get_session(request: Request):
+    return request.session
+@app.post('/login-check')
+async def login(username: str=Form(), password: str=Form(), session: dict = Depends(get_session)):
+    if username in users and users[username]["password"] == password:
+        session["user"] = username  # Store username in session upon successful login
         return Response(status_code=200)
     else:
-        raise HTTPException(status_code=401)
+        raise HTTPException(status_code=403)
 
 @app.get('/login')
 async def login_page():
     with open("./login-page.html", "r") as file:
         login_html = file.read()
     return HTMLResponse(content=login_html)
+
+# Dashboard endpoint (requires authentication)
+@app.get('/dashboard')
+async def dashboard(request: Request):
+    user = request.session.get("user")
+    if not user:
+        return RedirectResponse(url='/login')
+    admin_cursor.execute(f"SHOW TABLES");
+    table_list=admin_cursor.fetchall();
+    # Only accessible if authenticated
+    return templates.TemplateResponse('dashboard.html',{"request": request, "tables":table_list})
+
