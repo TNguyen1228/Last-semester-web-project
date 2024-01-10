00f946363dfc250fcb02c22e05d387ecd185a46c
TNguyen1228 <tuan1056@gmail.com>
2024-01-11 00:48:47 +0700

    Update coffee_management.sql

--- data/coffee_management.sql
+++ data/coffee_management.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 28, 2023 at 09:39 AM
+-- Generation Time: Jan 10, 2024 at 06:48 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -30,7 +30,7 @@ SET time_zone = "+00:00";
 CREATE TABLE `customers` (
   `customer_id` varchar(20) NOT NULL,
   `phone` varchar(255) DEFAULT NULL,
-  `total_spent` decimal(10,2) DEFAULT NULL
+  `total_spent` decimal(10,2) DEFAULT 0.00
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -38,8 +38,15 @@ CREATE TABLE `customers` (
 --
 
 INSERT INTO `customers` (`customer_id`, `phone`, `total_spent`) VALUES
-('ID_02672133096', '02672133096', 24.72),
-('ID_0349292753', '0349292753', 40.00);
+('ID_02672133096', '02672133096', 43.22),
+('ID_0349292753', '0349292753', 44.85),
+('ID_0934833472', '0934833472', 15.00),
+('ID_0943635243', '0943635243', 0.00),
+('ID_0976442532', '0976442532', 0.00),
+('ID_0987458552', '0987458552', 380.00),
+('ID_0987663232', '0987663232', 0.00),
+('ID_0987799636', '0987799636', 17.20),
+('ID_0987799637', '0987799637', 27.60);
 
 -- --------------------------------------------------------
 
@@ -51,11 +58,19 @@ CREATE TABLE `customers_booking` (
   `id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
   `phone` varchar(255) NOT NULL,
-  `person` int(11) NOT NULL,
+  `person` varchar(255) NOT NULL,
   `reservation_date` date NOT NULL,
   `time` varchar(255) NOT NULL,
   `message` text DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `customers_booking`
+--
+
+INSERT INTO `customers_booking` (`id`, `name`, `phone`, `person`, `reservation_date`, `time`, `message`) VALUES
+(11, 'Nguyễn Mạnh Tuấn', '+84349292753', 'Less than 5 person', '2024-01-08', '09:00 PM', NULL),
+(13, 'Lam Le', '20231217', 'Less than 5 person', '2024-01-08', '08:00 AM', NULL);
 
 -- --------------------------------------------------------
 
@@ -77,8 +92,9 @@ CREATE TABLE `employees` (
 --
 
 INSERT INTO `employees` (`employee_id`, `name`, `position`, `contact_info`, `salary`, `hire_date`) VALUES
-('ID_057674', 'Nicole Bauch', 'Servant', '0554882542', 200.00, '2023-01-10'),
-('ID_190391', 'Miss May Schneider IV', 'Bartender', '0178541449', 250.00, '2020-11-19'),
+('ID_000000', 'Duy Long Ng', 'Receptionist', '0938233233', 220.00, '2024-01-07'),
+('ID_444435', 'Levi', 'Bartender', '0987668372', 666.00, '2024-02-01'),
+('ID_444444', 'Sofm', 'Manager', '0999879765', 998.00, '0000-00-00'),
 ('ID_448450', 'Ms. Marcus Connelly', 'Manager', '0828129535', 200.00, '2020-12-12'),
 ('ID_474166', 'Pauline Hand', 'Servant', '0349819366', 200.00, '2022-11-11'),
 ('ID_739142', 'Lonnie Bahringer', 'Servant', '0342592971', 150.00, '2023-01-01'),
@@ -109,6 +125,9 @@ INSERT INTO `menu` (`item_id`, `item_name`, `description`, `price`, `local_url`)
 ('CF_Cortado', 'Cortado', 'Balanced with a splash of warm, silky milk, creating a harmonious and flavorful coffee indulgence.', 3.45, './assets/images/cortado.png'),
 ('CF_Latte', 'Latte(Hot/Iced)', 'A velvety blend of espresso and steamed milk, crowned with a delicate layer of foam, creating a creamy and satisfying coffee indulgence', 1.97, './assets/images/latte.png'),
 ('CF_Macchiato', 'Macchiato', 'Symphony of espresso perfection adorned with a touch of velvety foam, delivering a bold and rich flavor in every enticing sip.', 2.50, './assets/images/macchiato.png'),
+('other_bottle_beer', 'Bottle beer', 'Each bottle holding a story of brewing expertise and tradition. Its convenience and portability make bottled beer a beloved choice for enthusiasts, allowing them to savor their favorite brews anywhere, anytime.', 0.82, './assets/images/other_bottle_beer.jpg'),
+('other_mixed_fruit', 'Mixed Fruit', 'A vibrant medley of flavors, creating a delightful and refreshing treat for any occasion and any season', 5.00, './assets/images/other_mixed_fruit.jpg'),
+('other_sunflower_seeds', 'Sunflower Seeds', 'Sunflower seeds add a delightful crunch and nutty flavor to your offerings', 0.60, './assets/images/other_sunflower_seeds.jpg'),
 ('smoothie_apple_pie', 'Apple Pie Smoothie', 'Combining fresh apple slices, warm cinnamon and oats, mixed with yogurt or almond milk, creating a comforting, spiked concoction.', 2.60, './assets/images/smoothie_apple_pie.jpg'),
 ('smoothie_coconut', 'Coconut Smoothie', 'A tropical oasis in a glass, combining creamy coconut milk or water with chunks of fresh fruit, offering a delightful balance of sweetness and nutty richness.', 2.26, './assets/images/smoothie_coconut.jpg'),
 ('smoothie_Mango', 'Mango Smoothies', 'A tropical delight, blending ripe, juicy mangoes with creamy yogurt or non-dairy alternatives, often enhanced with a splash of citrus or honey for a sweet-tangy fusion.', 2.10, './assets/images/smoothie_mango.jpg'),
@@ -145,7 +164,32 @@ CREATE TABLE `orders` (
 
 INSERT INTO `orders` (`order_id`, `customer_id`, `order_date`, `total_amount`) VALUES
 ('23122815361002672133096', 'ID_02672133096', '2023-12-28', 24.72),
-('2312281538120349292753_102', 'ID_0349292753', '2023-12-28', 40.00);
+('2312281538120349292753_102', 'ID_0349292753', '2023-12-28', 40.00),
+('2401012210470349292753_101', 'ID_0349292753', '2024-01-01', 0.10),
+('2401041805020987458552', 'ID_0987458552', '2024-01-04', 80.00),
+('2401041807430987458552', 'ID_0987458552', '2024-01-04', 75.00),
+('2401041809040987458552', 'ID_0987458552', '2024-01-04', 75.00),
+('2401041809170987458552', 'ID_0987458552', '2024-01-04', 75.00),
+('24010518030702672133096', 'ID_02672133096', '2024-01-05', 11.00),
+('2401051803400987799637', 'ID_0987799637', '2024-01-05', 12.40),
+('2401051803580987799636', 'ID_0987799636', '2024-01-05', 17.20),
+('2401051804280934833472', 'ID_0934833472', '2024-01-05', 15.00),
+('24010520391202672133096', 'ID_02672133096', '2024-01-05', 7.50),
+('2401072017330987799637', 'ID_0987799637', '2024-01-07', 15.20),
+('2401090011540987458552', 'ID_0987458552', '2024-01-09', 75.00),
+('2401102309230349292753', 'ID_0349292753', '2024-01-10', 4.75);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `order_items`
+--
+
+CREATE TABLE `order_items` (
+  `order_id` varchar(30) NOT NULL,
+  `order_item` varchar(255) NOT NULL,
+  `quantity` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -166,7 +210,7 @@ CREATE TABLE `room_items` (
 --
 
 INSERT INTO `room_items` (`room_number`, `item_name`, `quantity`, `item_condition`, `last_checked`) VALUES
-(101, 'Amplifier', 1, 'Good', '2023-12-27'),
+(101, 'Amplifier', 1, 'Fair', '2024-04-01'),
 (101, 'Lighting System', 2, 'Good', '2023-12-17'),
 (101, 'Microphone', 4, 'Good', '2023-12-20'),
 (101, 'Monitor', 1, 'Excellent', '2023-12-19'),
@@ -199,6 +243,13 @@ CREATE TABLE `room_management` (
   `time_in` time NOT NULL,
   `time_out` time DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `room_management`
+--
+
+INSERT INTO `room_management` (`room_number`, `date`, `time_in`, `time_out`) VALUES
+(104, '2024-01-07', '20:11:50', '22:00:00');
 
 -- --------------------------------------------------------
 
@@ -262,6 +313,13 @@ ALTER TABLE `orders`
   ADD KEY `customer_id` (`customer_id`);
 
 --
+-- Indexes for table `order_items`
+--
+ALTER TABLE `order_items`
+  ADD PRIMARY KEY (`order_item`),
+  ADD KEY `order_id` (`order_id`);
+
+--
 -- Indexes for table `room_items`
 --
 ALTER TABLE `room_items`
@@ -288,7 +346,7 @@ ALTER TABLE `room_menu`
 -- AUTO_INCREMENT for table `customers_booking`
 --
 ALTER TABLE `customers_booking`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- Constraints for dumped tables
@@ -298,7 +356,13 @@ ALTER TABLE `customers_booking`
 -- Constraints for table `orders`
 --
 ALTER TABLE `orders`
-  ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`customer_id`) REFERENCES `customers` (`customer_id`);
+  ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`customer_id`) REFERENCES `customers` (`customer_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `order_items`
+--
+ALTER TABLE `order_items`
+  ADD CONSTRAINT `order_items_ibfk_1` FOREIGN KEY (`order_id`) REFERENCES `orders` (`order_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `room_items`
